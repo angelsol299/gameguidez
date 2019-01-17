d41f16406acb290736fc90fc576f38fe477e952c
angelsol299 <angelsol299@hotmail.com>
2019-01-17 22:28:23 +0100

    auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -2,12 +2,13 @@
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", e => {
   e.preventDefault();
+  //get user info
+
+  const email = signupForm["signup-email"].value;
+  const password = signupForm["signup-password"].value;
+
+  // signup user
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred);
+  });
 });
-
-//get user info
-
-const email = signupForm["signup-email"].value;
-const password = signupForm["signup-password"].value;
-
-// signup user
-auth.createUserWithEmailAndPassword(email, password);
