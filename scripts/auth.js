14dad7cf508828f778e79d665dcc2018ebdd0f1b
angelsol299 <angelsol299@hotmail.com>
2019-01-17 22:53:35 +0100

    auth.js signout

--- scripts/auth.js
+++ scripts/auth.js
@@ -30,4 +30,8 @@ const loginForm = document.querySelector("#login-form");
 loginForm.addEventListener("submit", e => {
   e.preventDefault();
   //get user info
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+
+  auth.signInWithEmailAndPassword(email, password);
 });
