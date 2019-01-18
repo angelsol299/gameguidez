1990f5f841843bcad6b08d155ec31f1094aee101
angelsol299 <angelsol299@hotmail.com>
2019-01-18 23:08:00 +0100

    auth.js login

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,10 @@ const guideList = document.querySelector(".guides");
 //setup guides
 const setupGuides = data => {
   let html = "";
-  data.forEach(doc => {});
+  data.forEach(doc => {
+    const guide = doc.data();
+    console.log(guide);
+  });
 };
 
 // setup materialize components
