a32f318f61ed3e289628ebd52afad71c9d07e859
angelsol299 <angelsol299@hotmail.com>
2019-01-21 20:19:31 +0100

    auth.js login

--- scripts/auth.js
+++ scripts/auth.js
@@ -43,6 +43,7 @@ signupForm.addEventListener("submit", e => {
 
   // signup user
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    return db.collection("users").doc(cred.user.uid);
     const modal = document.querySelector("#modal-signup");
     M.Modal.getInstance(modal).close();
     signupForm.reset();
